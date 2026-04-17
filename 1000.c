19b297dac74eaf7c0b3085556597f1dda8486795
ThiagoJKTakeda <thiago.takeda@unifesp.br>
2026-04-17 01:46:55 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Thiago Jun Kimura Takeda
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : DD/MM/2026
+Objetivo    : Imprimir Hello World.
+Aprendizado : Aprender o comando print
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int main()
