02133a82c397b2046da54ea268506c7be69e9e90
ThiagoJKTakeda <thiago.takeda@unifesp.br>
2026-04-17 01:52:28 -0300

    Update 1080.c

--- 1080.c
+++ 1080.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Thiago Jun Kimura Takeda
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
+Data        : 16/04/2026
+Objetivo    : Imprimir o maior de 100 números
+Aprendizado : Como trabalhar com vetores
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main()
