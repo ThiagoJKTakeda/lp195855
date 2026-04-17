349501d9372eda63b0ba6c4a443af214674a347f
ThiagoJKTakeda <thiago.takeda@unifesp.br>
2026-04-17 01:54:27 -0300

    Update 1071.c

--- 1071.c
+++ 1071.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Thiago Jun Kimura Takeda
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1071
+Data        : 15/04/2026
+Objetivo    : Ler dois valores inteiros e mostrar a soma dos ímpares entre eles
+Aprendizado : Funcionamento do comando while
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main()
