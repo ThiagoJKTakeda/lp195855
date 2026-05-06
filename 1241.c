c2302c99ad07f0e0223f3d42c1061cb3193eeb14
ThiagoJKTakeda <thiago.takeda@unifesp.br>
2026-05-06 01:27:19 -0300

    Create 1241.c

---
+++ 1241.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int N=0, i, t, tamanhoA=0,tamanhoB=0, fa=0, o=0, fb=0, g=0, c=0;
+    char A[1001];
+    char B[1001];
+    char C[1001];
+    scanf("%d ", &N);
+    for(i=0;i<N;i++){
+        tamanhoA=0;
+        tamanhoB=0;
+        t=0;
+        c=0;
+        o=0;
+        scanf("%s", A);
+        fa = strlen(A);
+        scanf("%s", B);
+        fb = strlen(B);
+        if(fa<fb){
+            printf("nao encaixa\n");
+        }
+        else if(fa>=fb){
+            g = fa-fb;
+            while(g<fa){
+                
+                C[c]= A[g];
+                c=c+1;
+                g=g+1;
+            }
+            C[c]='\0';
+            if(strcmp(C,B)==0){
+                printf("encaixa\n");
+            }
+            else{
+                printf("nao encaixa\n");
+            }
+            
+        }
+    }
+
+    return 0;
+}
