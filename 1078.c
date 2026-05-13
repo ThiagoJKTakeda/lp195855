9078977e41fd2920ce2fad68d897e14e7555da33
ThiagoJKTakeda <thiago.takeda@unifesp.br>
2026-05-12 21:38:13 -0300

    Update 1078.c

--- 1078.c
+++ 1078.c
@@ -3,7 +3,7 @@ Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Thiago Jun Kimura Takeda
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1078
-Data        : DD/MM/2026
+Data        : 28/04/2026
 Objetivo    : Escrever a tabuada de um número N
 Aprendizado : Aprender a usar o for
 -------------------------------------------------------------------------- */
