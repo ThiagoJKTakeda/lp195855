4ce5fcaa13382c91346aa7e317b6888a68289e13
ThiagoJKTakeda <thiago.takeda@unifesp.br>
2026-05-12 21:37:43 -0300

    Update 1234.c

--- 1234.c
+++ 1234.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Thiago Jun Kimura Takeda
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1234
+Data        : DD/MM/2026
+Objetivo    : Alternar entre maiúsculo e minusculo as letras de uma frase
+Aprendizado : Aprender comandos de string
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
