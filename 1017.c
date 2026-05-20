7efa5e5c65cea1b88f60b9e07af41be9031e8d6e
ThiagoJKTakeda <thiago.takeda@unifesp.br>
2026-05-20 00:04:42 -0300

    Update 1017.c

--- 1017.c
+++ 1017.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Thiago Jun Kimura Takeda
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1017
+Data        : 20/05/2026
+Objetivo    :Calcular quantos litros um carro consome.
+Aprendizado : A trabalhar com vetores.
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 float calcularlitros(int tempo, int velocidade){
