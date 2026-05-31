ad1823534612a136ed524b01f5b4d95e5d013855
ThiagoJKTakeda <thiago.takeda@unifesp.br>
2026-05-31 01:57:44 -0300

    Update 1441.c

--- 1441.c
+++ 1441.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Thiago Jun Kimura Takeda
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1441
+Data        : 31/05/2026
+Objetivo    : A partir de um número h, gerar uma sequêncoa até chegar em 1 e falar qual foi o maior número da sequência.
+Aprendizado : A trabalhar com funções recursivas.
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 int maior = 0, n;
 int gran(int h){
