7044815e556452376b64dc046728f72ea0a6517f
ThiagoJKTakeda <thiago.takeda@unifesp.br>
2026-05-31 00:04:00 -0300

    Update 1029.c

--- 1029.c
+++ 1029.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Thiago Jun Kimura Takeda
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1029
+Data        : 30/05/2026
+Objetivo    : Calcular a série de fibonacci contando quantas chamadas recursivas foram feitas.
+Aprendizado : A trabalhar com funções recursivas e contar a quantidade de chamadas recursivas.
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 int contador;
