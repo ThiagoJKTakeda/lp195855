70dd9ac94387d8fbd20d1f5e29541953d5b8adf8
ThiagoJKTakeda <thiago.takeda@unifesp.br>
2026-05-31 02:35:32 -0300

    Update 1169.c

--- 1169.c
+++ 1169.c
@@ -1,3 +1,13 @@
+V/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Thiago Jun Kimura Takeda
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1169
+Data        : 31/05/2026
+Objetivo    :Calcular a massa grãos a partir das casas de um tabuleiro.
+Aprendizado : A trabalhar com funções recursivas.
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 unsigned long long potencia(int elevado){
