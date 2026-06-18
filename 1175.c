588546e8fd5d673abe12589f3f27548971c566e5
ThiagoJKTakeda <thiago.takeda@unifesp.br>
2026-06-18 12:46:43 -0300

    Add header comments to 1175.c
    Added header comments with problem details and objectives.

--- 1175.c
+++ 1175.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Thiago Jun Kimura Takeda
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1175
+Data        : 18/06/2026
+Objetivo    :Inverter os valores de um vetor.
+Aprendizado : A trabalhar com vetores.
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main()
