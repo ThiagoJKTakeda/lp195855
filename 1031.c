cfe78ee0952aa739aaebe78fbbc440b53f54ea87
ThiagoJKTakeda <thiago.takeda@unifesp.br>
2026-06-17 22:06:47 -0300

    Update 1031.c

--- 1031.c
+++ 1031.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Thiago Jun Kimura Takeda
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1031
+Data        : 17/06/2026
+Objetivo    :Encontrar quantas unidades pular para que a região 13 seja sempre a última a ser desligada.
+Aprendizado : A trabalhar com funções recursivas.
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int corte(int N, int m){
